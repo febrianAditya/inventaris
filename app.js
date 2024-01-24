b65818eaa5db0cdbfeaa2941e104f56ecbb586ae
FebrianAditya <febrian.aditya@student.upi.edu>
2024-01-25 01:11:15 +0700

    add env PORT

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port} ${process.env.NODE_ENV} ${process.env.PORT}`)
 })
